4e318cd32ada758c31f49b69b005be4e4a5b28f4
Mariia Chmurova <kkettch@gmail.com>
2025-01-21 13:14:43 +0300

    Update Game.fs

--- LAB4/Game.fs
+++ LAB4/Game.fs
@@ -14,19 +14,7 @@ open EnemyBehaviour
 open PlatformBehaviour
 open CharacterBehaviour
 
-let finishText = 
-    let winText = Text("You Win!", font, 60u) 
-    winText.FillColor <- Color.Green
-    winText.Position <- Vector2f(float32 windowWidth / 2.0f - 100.0f, float32 windowHeight / 2.0f - 50.0f) 
-    let timetText = Text(sprintf "Time: %.2f min" stopwatch.Elapsed.TotalMinutes, font, 50u)
-    timetText.FillColor <- Color.Green
-    timetText.Position <- Vector2f(float32 windowWidth / 2.0f - 120.0f, float32 windowHeight / 2.0f) // Центр окна
-    window.Clear(Color.White)
-    window.Draw(winText)
-    window.Draw(timetText)
-    window.Display()
-
-let clock = Clock()
+let clock = Clock() 
 
 let rec gameLoop (state: GameState) =
     if window.IsOpen then
@@ -47,13 +35,15 @@ let rec gameLoop (state: GameState) =
                 Coins = state.Coins
                 CollectedCoins = state.CollectedCoins
                 Enemies = state.Enemies 
+                CoinAnimation = state.CoinAnimation
             }
             gameLoop newState
     
         else
             let (newPosition, newVerticalSpeed, newIsJumping) = 
                 updatePosition state.Position state.VerticalSpeed state.isJumping horizontalShift
-          
+
+            let updatedCoinAnimation = updateCoinAnimation state.CoinAnimation deltaTime
             let updatedEnemies = moveEnemies state.Enemies platforms
             
             if checkEnemyCollision newPosition squareSize updatedEnemies then
@@ -63,11 +53,11 @@ let rec gameLoop (state: GameState) =
             let remainingCoins, collected = checkCoinCollision newPosition squareSize state.Coins
             let newCollectedCoins = state.CollectedCoins + collected
 
-            window.Clear(Color.White)
+            window.Clear(Color.White)   
 
             window.Draw(coinCounter (finishCoins - newCollectedCoins))
             drawPlatforms window
-            drawCoins window remainingCoins deltaTime
+            drawCoins window remainingCoins state.CoinAnimation deltaTime
             drawEnemies window updatedEnemies 
 
             let doorShape = RectangleShape(Size = door.Size,Position = door.Position)
@@ -84,7 +74,16 @@ let rec gameLoop (state: GameState) =
 
             if checkDoorCollisison newPosition squareSize doorShape && doorShape.FillColor = Color.Green then
                 stopwatch.Stop()
-                finishText
+                let winText = Text("You Win!", font, 60u) 
+                winText.FillColor <- Color.Green
+                winText.Position <- Vector2f(float32 windowWidth / 2.0f - 100.0f, float32 windowHeight / 2.0f - 50.0f) 
+                let timetText = Text(sprintf "Time: %.2f min" stopwatch.Elapsed.TotalMinutes, font, 50u)
+                timetText.FillColor <- Color.Green
+                timetText.Position <- Vector2f(float32 windowWidth / 2.0f - 120.0f, float32 windowHeight / 2.0f) // Центр окна
+                window.Clear(Color.White)
+                window.Draw(winText)
+                window.Draw(timetText)
+                window.Display()
                 Thread.Sleep(3000)
                 window.Close()
   
@@ -94,6 +93,7 @@ let rec gameLoop (state: GameState) =
                 isJumping = newIsJumping
                 Coins = remainingCoins
                 CollectedCoins = newCollectedCoins
-                Enemies = updatedEnemies 
+                Enemies = updatedEnemies
+                CoinAnimation = updatedCoinAnimation 
             }
             gameLoop newState
